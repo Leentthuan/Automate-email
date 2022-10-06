22e857aeae6f8a1af81e05bf04d54a298c39c7b1
Leentthuan <115142814+Leentthuan@users.noreply.github.com>
2022-10-06 15:05:49 +0700

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import smtplib
+import ssl
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.message import EmailMessage
+
+# Attach message body content
+# Create the body of the message (a plain-text and an HTML version).
+df = pd.read_excel('/Users/user1/Desktop/Sheet2.xlsx')
+df['Duration'] = df['Duration'].dt.strftime('%B %d, %Y')
+
+for r in df.itertuples():      
+        First_name = r[1]
+        Name = r[2]
+        Recipient = r[3]
+        Description = r[4]
+        Discount_code = r[5]
+        Discount_amount = r[6]
+        Duration = r[7]
+        Email_sent = r[8]
+
+        msg = f"""
+        Dear {First_name},
+        <br>Here is  your {Discount_code} for {Description}.
+        <br>Remember to use it before {Duration}. 
+        <br>Best regard,
+        <br>Lee
+
+        """
+        print(msg)
+        # Define the transport variables
+        ctx = ssl.create_default_context()
+        password = "Your app password"    # Your app password goes here
+        sender = "Youremail@gmail.com"   # Your e-mail address
+        receiver = Recipient # Recipient's address
+        print(receiver)
+        
+        # Create the message
+        message = MIMEMultipart("mixed")
+        message["From"] = sender
+        message["To"] = receiver
+        message["Subject"] = "Testing things!"
+
+        # Record the MIME types of both parts - text/plain and text/html.
+        #part1 = MIMEText(text, 'plain')
+        part1 = MIMEText(msg, 'html')
+
+        # Attach parts into message container.
+        # According to RFC 2046, the last part of a multipart message, in this case
+        # the HTML message, is best and preferred.
+        #message.attach(part1)
+        message.attach(part1)
+
+        # Attach image
+        ##filename = '/Users/user1/Desktop/Sheet2.xlsx'
+        ##with open(filename, "rb") as f:
+        ##    file = MIMEApplication(f.read())
+                
+        ##disposition = f"attachment; filename={filename}"
+        ##file.add_header("Content-Disposition", disposition)
+        ##message.attach(file)
+
+        # Connect with server and send the message
+        with smtplib.SMTP_SSL("smtp.gmail.com", port=465, context=ctx) as server:
+                server.login(sender, password)
+                server.sendmail(sender, receiver, message.as_string())
+                server.quit()
+                print("Mail sent!")
